17a480d0a5ff2283aec4dd8977ecb3f9992748e7
Nikita <ndemidovn@gmail.com>
2022-11-08 15:09:07 +0300

    enum

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 "use strict";
-const skills = [1, 'dev'];
+var statusCode;
+(function (statusCode) {
+    statusCode[statusCode["SUCCESS"] = 1] = "SUCCESS";
+    statusCode["IN_PROCESS"] = "p";
+    statusCode["FAILED"] = "f";
+})(statusCode || (statusCode = {}));
+const res = {
+    message: 'Платеж успешен',
+    statusCode: statusCode.SUCCESS
+};
+// const SUCCESS = 'success'
+// 1 - успех
+// 'p - в процессе
+// 'f - отклонен
+if (res.statusCode === statusCode.SUCCESS) {
+}
+function action(status) {
+}
+action(statusCode.SUCCESS);
+action(1);
+function compute() {
+    return 3;
+}
+var Roles;
+(function (Roles) {
+    Roles[Roles["ADMIN"] = 1] = "ADMIN";
+    Roles[Roles["USER"] = compute()] = "USER";
+})(Roles || (Roles = {}));
--- app.ts
+++ app.ts
@@ -1,6 +1,38 @@
-const skills: readonly [number, string] = [1, 'dev'];
+enum statusCode {
+  SUCCESS = 1,
+  IN_PROCESS = 'p',
+  FAILED = 'f'
+}
 
-// const skill: readonly string[] = ['dev', 'devops'];
+const res = {
+  message: 'Платеж успешен',
+  statusCode: statusCode.SUCCESS
+};
 
-const skill: ReadonlyArray<string> = ['dev', 'devops'];
+
+
+// const SUCCESS = 'success'
+
+// 1 - успех
+// 'p - в процессе
+// 'f - отклонен
+
+if(res.statusCode === statusCode.SUCCESS){
+
+}
+function action(status: statusCode){
+
+}
+
+action(statusCode.SUCCESS);
+action(1);
+
+function compute(){
+  return 3;
+}
+
+enum Roles {
+  ADMIN = 1,
+  USER = compute()
+}
 
